8364ef16bbbcb2c6968b8a8862967e11f5ed55c6
paulo <pauloh16.pdr@gmail.com>
2020-04-30 14:31:18 -0300

    Primeira calsse 'posicao'

---
+++ src/tabuleiro/Posicao.java
@@ -0,0 +1,44 @@
+package tabuleiro;
+
+public class Posicao
+{
+	private int linha, coluna;
+
+	//Construtor
+	public Posicao(int linha, int coluna)
+	{
+		this.setLinha(linha);
+		this.setColuna(coluna);
+	}
+
+	//Getters
+	public int getLinha()
+	{
+		return this.linha;
+	}
+
+	public int getColuna()
+	{
+		return this.coluna;
+	}
+
+	//Setters
+	public void setLinha(int linha)
+	{
+		this.linha = linha;
+	}
+
+	public void setColuna(int coluna)
+	{
+		this.coluna = coluna;
+	}
+
+	//métodos sobrescrevidos
+	@Override
+	public String toString()
+	{
+		return this.getLinha() + ", " + this.getColuna();
+	}
+
+	
+}
