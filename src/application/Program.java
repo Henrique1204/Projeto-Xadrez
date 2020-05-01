d223d9151b3ccf411941026865871a9cdb56c830
paulo <pauloh16.pdr@gmail.com>
2020-05-01 15:58:51 -0300

    Adicionar tratamento de excesspes e limpar tela

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import xadrez.PartidaXadrez;
 import xadrez.PecaXadrez;
 import xadrez.PosicaoXadrez;
+import xadrez.XadrezException;
 
 public class Program
 {
@@ -15,15 +17,28 @@ public class Program
 
 		while(true)
 		{
-			UI.printTabuleiro(partida.pegarPecas());
+			try {
+				UI.clearScreen();
+				UI.printTabuleiro(partida.pegarPecas());
 
-			System.out.print("\nOrigem: ");
-			PosicaoXadrez origem = UI.lerPosicaoXadrez(sc);
+				System.out.print("\nOrigem: ");
+				PosicaoXadrez origem = UI.lerPosicaoXadrez(sc);
 
-			System.out.print("\nDestino: ");
-			PosicaoXadrez destino = UI.lerPosicaoXadrez(sc);
+				System.out.print("\nDestino: ");
+				PosicaoXadrez destino = UI.lerPosicaoXadrez(sc);
 
-			PecaXadrez pecaCapturada = partida.executarMovimento(origem, destino);
+				PecaXadrez pecaCapturada = partida.executarMovimento(origem, destino);
+			}
+			catch (XadrezException e)
+			{
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e)
+			{
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
 }
